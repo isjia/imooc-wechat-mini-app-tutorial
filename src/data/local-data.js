814620bf9e6f3a871b2d1223bd8358a0ec608fd7
Jenny Qian <isjia@163.com>
2017-11-30 22:33:53 +0800

    finish notes for Ch05

---
+++ src/data/local-data.js
@@ -0,0 +1,36 @@
+var localData = [
+  {
+    date: "Nov 11, 2017",
+    avatar: '/images/avatar.jpg',
+    title: "我是标题",
+    imgSrc: "/images/post1.jpg",
+    content: "雖然說羅技在 IFA 發表的的 CRAFT 鍵盤和 MX Sound 喇叭稍早在 1111 的時候都已經偷偷搶先在線上通路開賣，但其實今天才是它們正式的發表會呢。CRAFT 鍵盤是羅技睽違已久的高階辦公室鍵盤新品，這次主要是瞄準了創意工作者。它左上角有一個類似於微軟 Surface Dial 或 Dell Totem 的「創意轉鈕」，搭配 Adobe CC、Office 等特定套裝軟體時，可以提供各種不一樣的功能。它最有趣之處，是它可以視需求，在滑順和「段格」式的旋轉模式之間切換 -- 例如縮放可以用滑順的，提供精確的縮放比例；而選擇主題的時候，則是可以一格一格的選，確保不會一下子轉過頭。整顆轉鈕可以向下按不說，上側表面還是觸控的，提供各種不同的操作可能。",
+    reading: 66,
+    collection: 99,
+    show: true
+  },
+  {
+    date: "Nov 12, 2017",
+    avatar: '/images/avatar.jpg',
+    title: "我是标题",
+    imgSrc: "/images/post2.jpg",
+    content: "雖然說羅技在 IFA 發表的的 CRAFT 鍵盤和 MX Sound 喇叭稍早在 1111 的時候都已經偷偷搶先在線上通路開賣，但其實今天才是它們正式的發表會呢。CRAFT 鍵盤是羅技睽違已久的高階辦公室鍵盤新品，這次主要是瞄準了創意工作者。它左上角有一個類似於微軟 Surface Dial 或 Dell Totem 的「創意轉鈕」，搭配 Adobe CC、Office 等特定套裝軟體時，可以提供各種不一樣的功能。它最有趣之處，是它可以視需求，在滑順和「段格」式的旋轉模式之間切換 -- 例如縮放可以用滑順的，提供精確的縮放比例；而選擇主題的時候，則是可以一格一格的選，確保不會一下子轉過頭。整顆轉鈕可以向下按不說，上側表面還是觸控的，提供各種不同的操作可能。",
+    reading: 89,
+    collection: 34,
+    show: true
+  },
+  {
+    date: "Nov 13, 2017",
+    avatar: '/images/avatar.jpg',
+    title: "我是标题",
+    imgSrc: "/images/post3.jpg",
+    content: "雖然說羅技在 IFA 發表的的 CRAFT 鍵盤和 MX Sound 喇叭稍早在 1111 的時候都已經偷偷搶先在線上通路開賣，但其實今天才是它們正式的發表會呢。CRAFT 鍵盤是羅技睽違已久的高階辦公室鍵盤新品，這次主要是瞄準了創意工作者。它左上角有一個類似於微軟 Surface Dial 或 Dell Totem 的「創意轉鈕」，搭配 Adobe CC、Office 等特定套裝軟體時，可以提供各種不一樣的功能。它最有趣之處，是它可以視需求，在滑順和「段格」式的旋轉模式之間切換 -- 例如縮放可以用滑順的，提供精確的縮放比例；而選擇主題的時候，則是可以一格一格的選，確保不會一下子轉過頭。整顆轉鈕可以向下按不說，上側表面還是觸控的，提供各種不同的操作可能。",
+    reading: 33,
+    collection: 66,
+    show: true
+  }
+]
+
+module.exports = {
+  posts: localData
+}
